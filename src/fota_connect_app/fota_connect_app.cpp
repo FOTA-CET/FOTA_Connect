f1160684643a86bcd4a59c1908c0cbe3a47ffb73
“TranThanhTung” <ttung6618@gmail.com>
2024-05-17 20:22:56 +0700

    rename something

--- src/fota_connect_app/fota_connect_app.cpp
+++ src/fota_connect_app/fota_connect_app.cpp
@@ -15,17 +15,21 @@
 
 fotaConnectApp::fotaConnectApp()
 {
-  if (std::getenv("FOTA_CONFIG_DIR") == nullptr) {
+  if (std::getenv("FOTA_CONFIG_DIR") == nullptr)
+  {
     auto errMsg = "fotaConnectApp: environment variable FOTA_CONFIG_DIR is not set";
     throw std::runtime_error(errMsg);
-  } else {
+  }
+  else
+  {
     fotaConfDir = std::getenv("FOTA_CONFIG_DIR");
     firmwaresMetadataFile = fotaConfDir + "/firmwareList.json";
     jsonkeyFile = fotaConfDir + "/firebaseConfig.json";
     tokenFile = fotaConfDir + "/tokenConfig.json";
   }
 
-  if (std::getenv("FOTA_STORAGE") == nullptr) {
+  if (std::getenv("FOTA_STORAGE") == nullptr)
+  {
     auto errMsg = "fotaConnectApp: environment variable FOTA_STORAGE is not set";
     throw std::runtime_error(errMsg);
   }
@@ -34,21 +38,24 @@ fotaConnectApp::fotaConnectApp()
     fotaStorage = std::getenv("FOTA_STORAGE");
     fifoECU = fotaStorage + "/fifoECU";
     fifoFlash = fotaStorage + "/fifoFlash";
+    fifoPercent = fotaStorage + "/fifoProgress";
     firmwareDir = fotaStorage; // + "/Firmware/";
   }
 }
 
-void fotaConnectApp::signalHandler(int signal) {
+void fotaConnectApp::signalHandler(int signal)
+{
   std::cout << "Received signal " << signal << std::endl;
   exit(signal);
 }
 
-bool fotaConnectApp::writeFifoPipe(const std::string& fifoPath, std::string& buff) {
-    mkfifo(fifoPath.c_str(), 0666);
-    int fd = open(fifoPath.c_str(), O_WRONLY);
-    write(fd, buff.c_str(), buff.length());
-    close(fd);
-    return true;
+bool fotaConnectApp::writeFifoPipe(const std::string &fifoPath, std::string &buff)
+{
+  mkfifo(fifoPath.c_str(), 0666);
+  int fd = open(fifoPath.c_str(), O_WRONLY);
+  write(fd, buff.c_str(), buff.length());
+  close(fd);
+  return true;
 }
 
 void fotaConnectApp::start()
@@ -61,48 +68,133 @@ void fotaConnectApp::start()
   jsonKey::handleFirebaseJson(jsonkeyFile);
   jsonKey::handleFirebaseToken(tokenFile);
 
-  while(object_fotaDownload.getNameFirmware(name) == Status::ERROR);
-  object_fotaDownload.setfirmwareMetadata(firmwaresMetadataFile);
-  std::cout << "Found new firmware:\t";
-  std::cout << name << std::endl;
-  std::cout << "Checking firmware\n";
-  if(object_fotaDownload.checkNewestState(name) != Status::OK)
+  while (1)
   {
-    std::cout << "Firmware already exists.\n";
-    restAdapter::resetStatusFirebase();
+    if (object_fotaDownload.getNameFirmware(name) == Status::OK)
+    {
+      object_fotaDownload.setfirmwareMetadata(firmwaresMetadataFile);
+
+      std::string ecuName = name.substr(0, name.find("_")); // regex
+      std::string filePath = firmwareDir + "/" + name;      // concat
+
+      std::cout << "Found new firmware:\t";
+      std::cout << name << std::endl;
+      std::cout << "Checking firmware\n";
+      if (object_fotaDownload.checkNewestState(name) != Status::OK)
+      {
+        std::cout << "Firmware already exists.\n";
+        if (object_fotaDownload.resetUpdateFieldFirebase())
+          std::cout << "Reset state OK" << std::endl;
+        else
+          std::cout << "Reset state ERROR\n"
+                    << std::endl;
+        if (object_fotaDownload.updateMCUStatus(ecuName, ECU_StatustoString(ECU_Status::REJECT)))
+          std::cout << "Update MCU status OK" << std::endl;
+        else
+          std::cout << "Update MCU status ERROR" << std::endl;
+      }
+      else
+      {
+        std::cout << "Check OK!\n";
+        if (strcmp(ecuName.c_str(), "ESP32") != 0)
+        {
+          filePath += ".hex";
+        }
+        else
+        {
+          filePath += ".bin";
+        }
+
+        std::string fileName = name + filePath.substr(filePath.length() - 4, 4); // luu y
+        if (object_fotaDownload.download(stringToECU(ecuName), name, filePath) == Status::OK)
+        {
+          object_fotaDownload.updateMCUStatus(ecuName, ECU_StatustoString(ECU_Status::DOWNLOAD));
+          std::cout << "Download success\n";
+          std::cout << "Sending FIFO\n";
+          writeFifoPipe(fifoECU, ecuName);
+          writeFifoPipe(fifoFlash, fileName);
+          std::cout << "Send successful" << std::endl;
+          object_fotaDownload.updateFirmwareList(name);
+          object_fotaDownload.resetUpdateFieldFirebase();
+        }
+        else
+        {
+          std::cout << "Download fails\n";
+        }
+      }
+    }
+    signal(SIGINT, fotaConnectApp::signalHandler);
+    signal(SIGTERM, fotaConnectApp::signalHandler);
   }
+}
+
+std::string fotaConnectApp::ECUtoString(ECU ecu)
+{
+  if (ecu == ECU::ESP32)
+    return "ESP32";
+  else if (ecu == ECU::ATMEGA328P)
+    return "ATMEGA328P";
   else
-  {
-    std::cout << "Check OK!\n";
-    std::string ecuName = name.substr(0,name.find("_"));
-    std::string filePath = firmwareDir + "/" + name;
-    if(object_fotaDownload.stringToECU(ecuName) != ECU::ESP32) 
-    {
-      filePath += ".hex";
-    }
-    else 
-    {
-      filePath += ".bin";
-    }
+    return "STM32";
+}
 
-    std::string fileName = name + filePath.substr(filePath.length() - 4, 4);
-    if(object_fotaDownload.download(object_fotaDownload.stringToECU(ecuName), name, filePath) == Status::OK)
-    {
-      std::cout << "Download success\n";
-      std::cout << "Sending FIFO\n";
-      writeFifoPipe(fifoECU, ecuName);
-      writeFifoPipe(fifoFlash,fileName);
-      std::cout << "Send sucessful" << std::endl;
-      object_fotaDownload.updateFirmwareList(name);
-      restAdapter::resetStatusFirebase();
-    }
-    else
-    {
-      std::cout << "Download fails\n";
-    }
+std::string fotaConnectApp::ECU_StatustoString(ECU_Status status)
+{
+  if (status == ECU_Status::NONE)
+    return "NONE";
+  else if (status == ECU_Status::DOWNLOAD)
+    return "DOWNLOAD";
+  else
+    return "REJECT";
+}
+
+ECU fotaConnectApp::stringToECU(std::string &ecu)
+{
+  if (!strcmp(ecu.c_str(), "ESP32"))
+    return ECU::ESP32;
+  else if (!strcmp(ecu.c_str(), "STM32"))
+    return ECU::STM32;
+  else
+    return ECU::ATMEGA328P;
+}
+
+bool fotaConnectApp::readFifoPipe(const std::string &fifoPath, std::string &buff)
+{
+  char buffer[100];
+  memset(buffer, 0, sizeof(buffer));
+  auto fd = open(fifoPath.c_str(), O_RDONLY);
+  if (fd == -1)
+  {
+    return false;
   }
 
-  signal(SIGINT, fotaConnectApp::signalHandler);
-  signal(SIGTERM, fotaConnectApp::signalHandler);
-  fotaConnectApp::start();
+  auto ret = read(fd, &buffer, sizeof(buffer));
+  if (ret <= 0)
+  {
+    return false;
+  }
+  buff = buffer;
+  close(fd);
+  return true;
+}
+
+void fotaConnectApp::handleProgress()
+{
+  while (1)
+  {
+    if (readFifoPipe(fifoPercent, percentBuf))
+    {
+      std::string percent = percentBuf.substr(percentBuf.find("_") + 1);
+      std::string ecu = percentBuf.substr(0, percentBuf.find("_"));
+      if (restAdapter::writeFirebase(ecu + ".json", "percent", percent))
+      {
+        std::cout << "update percent successful\n";
+      }
+
+      else
+      {
+        std::cout << "Update percent fail\n";
+      }
+    }
+  }
 }
--- src/fota_connect_app/fota_connect_app.h
+++ src/fota_connect_app/fota_connect_app.h
@@ -1,22 +1,31 @@
 #ifndef __FOTA_CONNECT_APP_
 #define __FOTA_CONNECT_APP_
 #include <string>
+#include "cloudUrl.h"
+#include <thread>
 
 class fotaConnectApp
 {
     private:
         static void signalHandler(int signal);
         bool writeFifoPipe(const std::string& fifoPath, std::string& buff);
+        bool readFifoPipe(const std::string& fifoPath, std::string& buff);
         std::string fotaStorage;
         std::string fifoECU;
         std::string fifoFlash;
+        std::string fifoPercent;
         std::string firmwareDir; 
         std::string fotaConfDir;
         std::string firmwaresMetadataFile;
         std::string jsonkeyFile;
         std::string tokenFile;
+        std::string percentBuf;
     public:
         fotaConnectApp();
         void start();
+        std::string ECUtoString(ECU ecu);
+        ECU stringToECU(std::string& ecu);
+        std::string ECU_StatustoString(ECU_Status status);
+        void handleProgress();
 };
 #endif // include guard
