a0fbedaf16a9d057708c8c3d8f270a1d0bd7ae75
“TranThanhTung” <ttung6618@gmail.com>
2024-05-17 20:21:31 +0700

    update thread feature

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 int main()
 {
   auto a = fotaConnectApp();
-  a.start();
+
+    std::thread thread1(&fotaConnectApp::start, &a);
+    std::thread thread2(&fotaConnectApp::handleProgress, &a);
+
+    thread1.join();
+    thread2.join();
   return 0;
 }
