7962a28a1949cfd4de90c4c149ced9bf64042a08
“TranThanhTung” <ttung6618@gmail.com>
2024-05-23 00:24:17 +0700

    add new thread

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@ int main()
   auto a = fotaConnectApp();
 
     std::thread thread1(&fotaConnectApp::start, &a);
-    std::thread thread2(&fotaConnectApp::handleProgress, &a);
+    std::thread thread2(&fotaConnectApp::handlefifoPercent, &a);
+    std::thread thread3(&fotaConnectApp::updateECUPercentList, &a);
 
     thread1.join();
     thread2.join();
+    thread3.join();
   return 0;
 }
