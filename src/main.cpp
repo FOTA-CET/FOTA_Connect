ee7a3cbadc586c518287ea7cc6d066e3d0f03766
“TranThanhTung” <ttung6618@gmail.com>
2024-06-02 14:57:29 +0700

    fix missed thread

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,12 @@ int main()
     std::thread thread2(&fotaConnectApp::handlefifoPercent, &a);
     std::thread thread3(&fotaConnectApp::updateECUPercentList, &a);
     std::thread thread4(&fotaConnectApp::handleUpdateTrigger, &a);
+    std::thread thread5(&fotaConnectApp::handlefifoStatus, &a);
 
     thread1.join();
     thread2.join();
     thread3.join();
     thread4.join();
+    thread5.join();
   return 0;
 }
